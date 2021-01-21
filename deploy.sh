80ed9801a00ebf6c8c8ec4e30a086995c9966653
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-01-21 12:20:39 +0900

    빌드 자동화 Shell Script 추가 & 백업본

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/apps/
+
+cd $REPOSITORY/SpringBoot/
+
+echo "> Git Pull"
+
+git pull
+
+echo "> Starting Build Project"
+
+./gradlew build
+
+echo "> Copy Build File"
+
+cp ./build/libs/*.jar $REPOSITORY/
+
+echo "> Check Running Application PID"
+
+CURRENT_PID=$(pgrep -f SpringBoot)
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+    echo "> Cannot Find Running Application"
+else
+    echo "> kill -2 $CURRENT_PID"
+    kill -2 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> Build New Application"
+
+JAR_NAME=$(ls $REPOSITORY/ |grep 'SpringBoot' | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME &
