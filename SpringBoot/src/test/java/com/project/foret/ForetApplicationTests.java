84e470b76174c6a34f81a85e3d6f9dd377436865
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-01-21 11:11:17 +0900

    SpringBoot 초기 세팅

--- SpringBoot/src/test/java/com/project/foret/ForetApplicationTests.java
+++ SpringBoot/src/test/java/com/project/foret/ForetApplicationTests.java
@@ -1,10 +1,10 @@
-package com.project.danggeun;
+package com.project.foret;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DanggeunApplicationTests {
+class ForetApplicationTests {
 
 	@Test
 	void contextLoads() {
