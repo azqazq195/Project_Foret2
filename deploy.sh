2473a81033111bfd22edfdd82f5796cf0e3bdf0e
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-01-21 17:57:12 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,21 +18,21 @@ cp ./build/libs/*.jar $REPOSITORY/
 
 echo "> Check Running Application PID"
 
-CURRENT_PID=$(pgrep -f SpringBoot)
+CURRENT_PID=$(pgrep -f foret)
 
 echo "$CURRENT_PID"
 
 if [ -z $CURRENT_PID ]; then
     echo "> Cannot Find Running Application"
 else
-    echo "> kill -2 $CURRENT_PID"
-    kill -2 $CURRENT_PID
+    echo "> kill -9 $CURRENT_PID"
+    kill -9 $CURRENT_PID
     sleep 5
 fi
 
 echo "> Build New Application"
 
-JAR_NAME=$(ls $REPOSITORY/ |grep 'SpringBoot' | tail -n 1)
+JAR_NAME=$(ls $REPOSITORY/ |grep 'foret' | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
