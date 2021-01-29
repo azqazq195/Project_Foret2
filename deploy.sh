7e7c748d70c8f52944e32797292a6b2d2f6b743d
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-01-29 12:43:50 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -37,4 +37,4 @@ JAR_NAME=$(ls $REPOSITORY/ |grep 'foret' | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 # nohup java -jar $REPOSITORY/$JAR_NAME &
-nohup java -jar $REPOSITORY/$JAR_NAME &
+java -jar $REPOSITORY/$JAR_NAME &
